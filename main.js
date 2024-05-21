12d6f3c9688c76b67ef6d0b31c55d218f8c9f692
asoolito <roblox890king890@gmail.com>
2024-05-21 17:37:46 +0500

    Text

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+// let text = prompt ("So'z kiriting")
+// console.log(text.length);
+// console.log(text.toLowerCase());
+let userName = prompt("Bironta so'z kiriting")
+let userLetter = prompt("Biror harf kiriting")
+if(userName.includes(userLetter)){
+    alert(`${userName} so'zida ${userLetter} bor`)
+}
+else{
+    alert(`${userName} so'zida ${userLetter} yo'q`)
+}
+
+
+
